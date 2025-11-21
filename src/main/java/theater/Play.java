f3134219bf074128f138a833b3bc5b81a30dda6f
Zirui Xu <xuzirui866@gmail.com>
2025-11-21 13:30:43 -0500

    Task1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,31 @@
 package theater;
 
+/**
+ * Represents a play with a name and a type.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
         this.type = type;
     }
 }
